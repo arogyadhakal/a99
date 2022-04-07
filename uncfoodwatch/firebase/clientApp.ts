4318f1b98b95f5198ded71058a8fcb96db805dc6
Arogya Dhakal <arogya@email.unc.edu>
2022-04-07 15:39:46 -0400

    Make Firebase Auth UI

--- uncfoodwatch/firebase/clientApp.ts
+++ uncfoodwatch/firebase/clientApp.ts
@@ -1,19 +1,42 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const clientCredentials = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
   };
 
-if (!firebase.getApps.length) {
+if (!firebase.apps.length) {
     firebase.initializeApp(clientCredentials)
 }
 
-export default firebase
+export default firebase;
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBdW3WKlDPS2A17M4QvasVH4cUn2FnpgoM",
+//   authDomain: "unc-food-tracker.firebaseapp.com",
+//   projectId: "unc-food-tracker",
+//   storageBucket: "unc-food-tracker.appspot.com",
+//   messagingSenderId: "234846499256",
+//   appId: "1:234846499256:web:0651dd118d560aeb229f4d",
+//   measurementId: "G-KJQ3ZRC6B7"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
